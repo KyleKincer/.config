6d3b683051a7154f64c0f54ab4eeaf37668c54b5
Kyle Kincer <kyle_kincer@sweetwater.com>
2023-12-27 17:07:08 -0500

    add debug keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<C-b>", ":lua require'dap'.toggle_breakpoint()<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-CR>", ":lua require'dap'.continue()<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-n>", ":lua require'dap'.step_over()<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-m>", ":lua require'dap'.step_into()<CR>", { noremap = true, silent = true })
