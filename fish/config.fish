5f1899c7feff393abfebb2da2b1bdfd84a2f003f
Kyle Kincer <kylekincer@gmail.com>
2025-03-16 14:08:57 -0400

    update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,15 @@
 set -g fish_greeting ""
 set -g fish_prompt_pwd_dir_length 0
-eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# Homebrew setup (conditional for macOS)
+if test (uname) = Darwin
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+end
+
+# Go setup
+set -gx GOPATH $HOME/go
+set -gx GOBIN $GOPATH/bin
+set -gx PATH $PATH $GOBIN
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
